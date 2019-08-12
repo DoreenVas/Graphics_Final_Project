36b4f2a5ca47acd4b623b19377a5dded17ceb33a
NadavSpitzer <NadavSpitzer@NADAV-PC>
2019-08-12 15:44:03 +0300

    Style level2.

--- src/WorldObjects/World.java
+++ src/WorldObjects/World.java
@@ -180,28 +180,28 @@ public class World {
                 "resources/pics/dark_floor.jpg",
                 Collidable.Type.stay));
         // ceiling
-        walls.add(new BlockWall(new Vector(-55,50,-80f),
+        walls.add(new BlockWall(new Vector(-55,70,-80f),
                 110,1,100,
                 "resources/pics/stars.jpg",
                 Collidable.Type.stay));
         // front wall
         walls.add(new BlockWall(new Vector(-55,-2,-80f),
-                110,52,1,
+                110,72,1,
                 "resources/pics/fire_texture.jpg",
                 Collidable.Type.stay));
         // back wall
         walls.add(new BlockWall(new Vector(-55,-2,-180),
-                110,52,1,
+                110,72,1,
                 "resources/pics/fire_texture.jpg",
                 Collidable.Type.stay));
         // right wall
         walls.add(new BlockWall(new Vector(54,-2,-80),
-                1,52,100,
+                1,72,100,
                 "resources/pics/fire_texture.jpg",
                 Collidable.Type.stay));
         // left wall
         walls.add(new BlockWall(new Vector(-54,-2,-80),
-                1,52,100,
+                1,72,100,
                 "resources/pics/fire_texture.jpg",
                 Collidable.Type.stay));
 
@@ -244,7 +244,7 @@ public class World {
     }
 
     public void moveToLevel2() {
-        Player.setPos(new Vector(0f, 0.5f, -85f));
+        Player.setPos(new Vector(0f, 1.5f, -85f));
         Player.resetCoordiantions();
     }
 }
